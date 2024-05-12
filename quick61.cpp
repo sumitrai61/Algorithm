31cf77036bf80cd1f77aa8bf9f5ceb03d0731e99
sumitrai61 <125114678+sumitrai61@users.noreply.github.com>
2024-05-12 19:03:15 +0530

    quick61.cpp

--- quick61.cpp
+++ quick61.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int partition(int arr[], int l, int h)
+int partition(int arr[], int start, int end)
 {
 
-	int pivot = arr[l];
+	int pivot = arr[start];
 
 	int count = 0;
-	for (int i = l + 1; i <= h; i++) {
+	for (int i = start + 1; i <= end; i++) {
 		if (arr[i] <= pivot)
 			count++;
 	}
 
-	int p = l + count;
-	swap(arr[p], arr[l]);
+	// Giving pivot element its correct position
+	int pivotIndex = start + count;
+	swap(arr[pivotIndex], arr[start]);
 
-	int i = l, j = h;
+	// Sorting left and right parts of the pivot element
+	int i = start, j = end;
 
-	while (i < p && j > p) {
+	while (i < pivotIndex && j > pivotIndex) {
 
 		while (arr[i] <= pivot) {
 			i++;
@@ -27,29 +29,29 @@ int partition(int arr[], int l, int h)
 			j--;
 		}
 
-		if (i < p && j > p) {
+		if (i < pivotIndex && j > pivotIndex) {
 			swap(arr[i++], arr[j--]);
 		}
 	}
 
-	return p;
+	return pivotIndex;
 }
 
-void quickSort(int arr[], int l, int h)
+void quickSort(int arr[], int start, int end)
 {
 
 	// base case
-	if (l >= h)
+	if (start >= end)
 		return;
 
 	// partitioning the array
-	int p = partition(arr, l, h);
+	int p = partition(arr, start, end);
 
 	// Sorting the left part
-	quickSort(arr, l, p - 1);
+	quickSort(arr, start, p - 1);
 
 	// Sorting the right part
-	quickSort(arr, p + 1, h);
+	quickSort(arr, p + 1, end);
 }
 
 int main()
