856a0fc2ea6e57742b1d3e768612eec696ea1f47
sumitrai61 <125114678+sumitrai61@users.noreply.github.com>
2024-05-12 23:03:10 +0530

    count61.cpp

--- count61.cpp
+++ count61.cpp
@@ -44,18 +44,18 @@ int main()
     int n;
     cin>>n;
 	int arr[n];
-    for(int i; i<n; i++){
+    for(int i=0; i<n; i++){
         cout << "Element " << i+1 << ": ";
         cin >> arr[i];  
     }
     cout<<"Array before sorting: ";
-    for (int i;i<n;i++){
+    for (int i=0;i<n;i++){
         cout<<arr[i]<< " "; 
     }
     cout<<endl;
 	count_sort(arr, n); 
     cout<<"Array after sorting: ";
-    for (int i;i<n;i++){
+    for (int i=0;i<n;i++){
         cout<<arr[i]<< " "; 
     }
 	return 0; 
